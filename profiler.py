fe689070d98b70a3e44a4f4eea73fc8338d43cb6
return0927 <bjleh0927@gmail.com>
2018-08-25 01:41:15 +0900

    * 2018.08.25 Updated TestCase Runner

--- profiler.py
+++ profiler.py
@@ -1,13 +1,21 @@
 from subprocess import Popen, PIPE, STDOUT
 from time import time
 
+_comm = 'python "11 전염병 역학 조사/main.py"'
+
 start = time()
-p = Popen(["python", "main.py"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-_output = p.communicate(input=b"""4 1000000
-3 2 5 3
-5 4 6 1
-9 5 7 3
-6 8 3 4""")[0]
+p = Popen(_comm, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+_output = p.communicate(input=b"""7 10
+5 6 2
+2 3 1
+1 3 3
+4 5 1
+4 5 3
+4 2 6
+3 2 5
+3 5 6
+5 1 5
+4 6 5""")[0]
 end = time()
 
 print(_output.decode())
